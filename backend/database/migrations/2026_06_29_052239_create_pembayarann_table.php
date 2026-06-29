f0badca1114cee48d244d9f49f9db6b46a1faa01
RDP <raehandhyaputra@gmail.com>
2026-06-29 12:27:43 +0700

    add migration tabel pembayaran

---
+++ backend/database/migrations/2026_06_29_052239_create_pembayarann_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pembayarann', function (Blueprint $table) {
+            $table->increments('id'); //primary key tabel pembayaran
+            $table->unsignedInteger('pemesanan_id');
+            $table->decimal('jumlah_bayar', 12, 2);
+            $table->string('metode_pembayaran', 30)->nullable();
+            $table->string('status', 20)->default('belum_lunas');
+            $table->dateTime('waktu_pembayaran')->nullable();
+            $table->timestamps();
+            $table->foreign('pemesanan_id')->references('id')->on('pemesanan')->onDelete('cascade'); //foreign key tabel pemesanan
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pembayarann');
+    }
+};
