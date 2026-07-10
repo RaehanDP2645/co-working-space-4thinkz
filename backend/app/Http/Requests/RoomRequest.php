7c6951dc08076486a81de77d434d679637b3a1e8
RDP <raehandhyaputra@gmail.com>
2026-07-10 18:53:43 +0700

    fix typo

--- backend/app/Http/Requests/RoomRequest.php
+++ backend/app/Http/Requests/RoomRequest.php
@@ -19,13 +19,13 @@ class RoomRequest extends FormRequest
         $isUpdate = $this->isMethod('put') || $this->isMethod('patch');
 
         return [
-            'nama_ruangan' => [$isUpdate ? 'sometimes' : 'required', 'string', 'max:100'],
-            'jenis_ruangan' => [$isUpdate ? 'sometimes' : 'required', 'string', 'in:Meeting Room, Private Office, Hot Desk, Event Space'],
-            'kapasitas' => [$isUpdate ? 'sometimes' : 'required', 'integer', 'min:1'],
-            'harga' => [$isUpdate ? 'sometimes' : 'required', 'numeric', 'min:0'],
-            'tingkat_privasi' => ['nullable', 'in:rendah, sedang, tinggi'],
-            'mendukung_presentasi' => ['nullable', 'boolean'],
-            'status' => ['nullable', 'in:tersedia, perbaikan, nonaktif'],
+            'nama_ruangan' => [$isUpdate ? 'sometimes' : 'required','string','max:100'],
+            'jenis_ruangan' => [$isUpdate ? 'sometimes' : 'required','string','in:Meeting Room,Private Office,Hot Desk,Event Space'],
+            'kapasitas' => [$isUpdate ? 'sometimes' : 'required','integer','min:1'],
+            'harga' => [$isUpdate ? 'sometimes' : 'required','numeric','min:0'],
+            'tingkat_privasi' => ['nullable','in:rendah,sedang,tinggi'],
+            'mendukung_presentasi' => ['nullable','boolean'],
+            'status' => ['nullable', 'in:tersedia,perbaikan,nonaktif'],
         ];
     }
 }
